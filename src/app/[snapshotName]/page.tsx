d5c91beb3fd373f5c3a6917ec432ad5e21e6237e
Mike DiDomizio <mikedidomizio@gmail.com>
2023-06-16 22:18:41 -0400

    Additional metadata to page

--- src/app/[snapshotName]/page.tsx
+++ src/app/[snapshotName]/page.tsx
@@ -6,14 +6,28 @@ import {getSpecificSnapshot} from "@/lib/getSpecificSnapshot";
 import {getSnapshot} from "@/api/getSnapshot";
 import {getImageByDimensions} from "@/lib/getImageByDimensions";
 import Image from "next/image";
+import Link from "next/link";
 
 type RouteParams = {
     snapshotName: string
 }
 
+type Image = {
+    src: string
+    width: number
+    height: number
+}
+
+type BuildsJson = {
+    buildUrl: string,
+    buildId: string,
+    buildNumber: number
+    images: Image[]
+}
+
 async function getPercy(snapshotName: string) {
     const builds = await fetchBuilds(process.env.PERCY_PROJECT_SLUG || '')
-    const images = [];
+    const buildsJson: BuildsJson[] = []
 
     for (let build of getApprovedBuilds(builds.data)) {
         const snapshots = await listSnapshots(build.id)
@@ -25,14 +39,22 @@ async function getPercy(snapshotName: string) {
             const snapshotAttr = await getSnapshot(snapshot.id)
             const image = getImageByDimensions((snapshotAttr as any).included)
 
-            if (image) {
-                images.push(image.attributes.url)
-            }
+            buildsJson.push({
+                buildUrl: build.attributes["web-url"],
+                buildId: build.id,
+                buildNumber: build.attributes["build-number"],
+                // we default because if the image is busted, we still want the build to render
+                images: [{
+                    src: image?.attributes.url || '',
+                    height: image?.attributes.height || 0,
+                    width: image?.attributes.width || 0
+                }]
+            })
         }
     }
 
     return {
-        images,
+        buildsJson,
     }
 }
 
@@ -44,8 +66,11 @@ export default async function Page({ params }: { params: RouteParams }) {
 
     return (
         <main className="flex min-h-screen p-24">
-            {percyData.images.map(image => {
-                return <Image key={image} src={image} alt="" width={600} height={400} />
+            {percyData.buildsJson.map((build) => {
+                return <div>
+                    <Link href={build.buildUrl} target="_blank">{build.buildNumber}</Link>
+                    <Image key={build.images[0].src} src={build.images[0].src} alt="" width={build.images[0].width} height={build.images[0].height} />
+                </div>
             })}
         </main>
     )
