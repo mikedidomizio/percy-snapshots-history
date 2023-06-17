1e23db05e62fb86db135e7873a7cef05123d3652
Mike DiDomizio <mikedidomizio@gmail.com>
2023-06-16 23:53:04 -0400

    Better UI for List

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -4,9 +4,11 @@ import Link from "next/link";
 import Image from "next/image";
 
 export const List = ({ percyData, snapshotName }: { percyData: any, snapshotName: string }) => {
+    const [fetching, setFetching] = useState(false)
     const [buildsJson, setBuildsJson] = useState(percyData.buildsJson)
     const [lastBuildId, setLastBuildId] = useState(percyData.lastBuildId)
     const handleFetchMore = async () => {
+        setFetching(true)
         const results = await fetch(`/api/get-builds/${lastBuildId}?snapshotName=${snapshotName}`)
         const json = await results.json()
         setBuildsJson([
@@ -14,15 +16,19 @@ export const List = ({ percyData, snapshotName }: { percyData: any, snapshotName
             ...json.buildsJson,
         ])
         setLastBuildId(json.lastBuildId)
+        setFetching(false)
     }
 
     return <div>
-        {buildsJson.map((build: any) => {
-            return <div style={{ float: "left", maxHeight: "500px", maxWidth: "400px" }} key={build.buildUrl}>
-                <Link href={build.buildUrl} target="_blank">{build.buildNumber}</Link>
-                <Image src={build.images[0].src} alt="" width={build.images[0].width} height={build.images[0].height} />
-            </div>
-        })}
-        <button onClick={handleFetchMore}>Fetch more</button>
+        <div className="grid gap-12 md:grid-cols-2 lg:grid-cols-4 mb-6">
+            {buildsJson.map((build: any) => {
+                return <div key={build.buildUrl}>
+                    <Link href={build.buildUrl} target="_blank">{build.buildNumber}</Link>
+                    <Image src={build.images[0].src} alt="" width={build.images[0].width} height={build.images[0].height} />
+                </div>
+            })}
+        </div>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled={fetching} onClick={handleFetchMore}>
+            {fetching ? 'Fetching' : 'Search 100 more builds'}</button>
     </div>
 }
