f94bdcae5584ebb108a4e55963779145c1d071e5
Mike DiDomizio <mikedidomizio@gmail.com>
2023-11-23 17:49:14 -0500

    Fix height adjustments for info to align them per row

--- src/components/PercySnapshotCard.tsx
+++ src/components/PercySnapshotCard.tsx
@@ -10,13 +10,13 @@ type PercySnapshotCardProps = {
 }
 
 export const PercySnapshotCard = ({ buildNumber, buildUrl, branchName, image, imageOnHover }: PercySnapshotCardProps) => {
-    return <div className="card card-compact w-96 bg-base-100 shadow-xl">
+    return <div className="card card-compact flex w-96 bg-base-100 shadow-xl">
         <figure className="bg-white">
             <Link href={buildUrl} target="_blank">
                 <PercyImage hoverImage={imageOnHover} baseImage={image} />
             </Link>
         </figure>
-        <div className="card-body">
+        <div className="h-[120px] content-end p-4">
             <h2 className="card-title">
                 <Link href={buildUrl} target="_blank">{buildNumber}</Link>
             </h2>
